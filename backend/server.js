35bc287750357255a27474bbc11e9b74940f68b3
Jason Guzman <tjason01@Hotmail.com>
2023-07-18 18:02:36 -0500

    [TAS-8] Created API Routes (#1)

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config()
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
+import userRoutes from './routes/userRoutes.js'
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Server is ready '));
+app.use('/api/users', userRoutes)
 
-app.listen(port, () => console.log('Server stared on port ${port}'));
+app.get('/', (req, res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
 
 // - **POST / api / users ** Register a user
 // - **POST / api / users / auth ** Authenticate a user and get token
