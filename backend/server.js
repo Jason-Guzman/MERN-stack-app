acb1af78a3b2b52df9d3dc2eeadce852b2fb8a25
Jason Guzman <tjason01@Hotmail.com>
2023-07-20 00:23:36 -0500

    MongoDB Implementation (#2)

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config()
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js'
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js'
 
+connectDB();
 const app = express();
 
-app.use('/api/users', userRoutes)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRoutes);
+
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
@@ -15,11 +21,3 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
-// - **POST / api / users ** Register a user
-// - **POST / api / users / auth ** Authenticate a user and get token
-// - **POST / api / users / logout ** - Logout user and clear cookie
-// - **POST / api / users / logout ** - Logout user and clear cookie
-// - **GET / api / users / profile ** Get user profile
-// - **PUT / api / users / profile ** - Update profile
