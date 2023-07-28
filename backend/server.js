b78eff77ff320a5a9fd5ac6400530eb9962a20e0
Jason Guzman <Tjason01@hotmail.com>
2023-07-27 23:11:33 -0500

    [TAS-21] Complete JWT Auth

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config()
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
@@ -11,10 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
-
-
 app.get('/', (req, res) => res.send('Server is ready'));
 
 app.use(notFound);
