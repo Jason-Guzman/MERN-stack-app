21aa71a9b07e6cba17d239017ddeaf92c9563519
Jason Guzman <tjason01@Hotmail.com>
2023-08-23 11:15:29 -0500

    Added Toastify Error to Frontend (#8)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Outlet />
     </>
   );
