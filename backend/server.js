eebd5d1e415b4586876d7b6bc014b1e63eee07ad
Jason Guzman <Tjason01@hotmail.com>
2023-09-07 21:52:54 -0500

    Production Preparation

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
@@ -13,9 +14,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use('/api/users', userRoutes);
-app.get('/', (req, res) => res.send('Server is ready'));
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+}
+else {
+    app.get('/', (req, res) => res.send('Server is ready'));
+}
+
+
 
 app.use(notFound);
 app.use(errorHandler);
